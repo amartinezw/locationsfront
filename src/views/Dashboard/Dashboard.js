b9c6637ffe4fe243c3e34fd27f63f47977ad0550
Abraham Martinez <amartinezw@agarcia.com.mx>
2019-10-04 12:21:26 -0500

    Revert "Merge pull request #3 from amartinezw/gperez"
    This reverts commit 2366cfee621bc2a6dc62b9bbe33f8318679cf14e, reversing
    changes made to a2d6d0170b77e1c76c8dc457d14f9c380abda65d.

--- src/views/Dashboard/Dashboard.js
+++ src/views/Dashboard/Dashboard.js
@@ -1,18 +1,18 @@
 import React from "react";
 // react plugin for creating charts
-//import ChartistGraph from "react-chartist";
+import ChartistGraph from "react-chartist";
 // @material-ui/core
 import { makeStyles } from "@material-ui/core/styles";
 import Icon from "@material-ui/core/Icon";
 // @material-ui/icons
 import Store from "@material-ui/icons/Store";
-//import Warning from "@material-ui/icons/Warning";
+import Warning from "@material-ui/icons/Warning";
 import DateRange from "@material-ui/icons/DateRange";
 import LocalOffer from "@material-ui/icons/LocalOffer";
-//import Update from "@material-ui/icons/Update";
-//import ArrowUpward from "@material-ui/icons/ArrowUpward";
-//import AccessTime from "@material-ui/icons/AccessTime";
-//import Accessibility from "@material-ui/icons/Accessibility";
+import Update from "@material-ui/icons/Update";
+import ArrowUpward from "@material-ui/icons/ArrowUpward";
+import AccessTime from "@material-ui/icons/AccessTime";
+import Accessibility from "@material-ui/icons/Accessibility";
 import BugReport from "@material-ui/icons/BugReport";
 import Code from "@material-ui/icons/Code";
 import Cloud from "@material-ui/icons/Cloud";
@@ -22,7 +22,7 @@ import GridContainer from "components/Grid/GridContainer.js";
 import Table from "components/Table/Table.js";
 import Tasks from "components/Tasks/Tasks.js";
 import CustomTabs from "components/CustomTabs/CustomTabs.js";
-//import Danger from "components/Typography/Danger.js";
+import Danger from "components/Typography/Danger.js";
 import Card from "components/Card/Card.js";
 import CardHeader from "components/Card/CardHeader.js";
 import CardIcon from "components/Card/CardIcon.js";
@@ -30,12 +30,12 @@ import CardBody from "components/Card/CardBody.js";
 import CardFooter from "components/Card/CardFooter.js";
 
 import { bugs, website, server } from "variables/general.js";
-//import RemoteTable from "components/RemoteTable/RemoteTable.jsx";
-/*import {
+import RemoteTable from "components/RemoteTable/RemoteTable.jsx";
+import {
   dailySalesChart,
   emailsSubscriptionChart,
   completedTasksChart
-} from "variables/charts.js";*/
+} from "variables/charts.js";
 
 import styles from "assets/jss/material-dashboard-react/views/dashboardStyle.js";
 
