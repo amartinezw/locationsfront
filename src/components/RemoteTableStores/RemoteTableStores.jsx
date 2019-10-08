db2990497689161936afad4a26dd29bf5e126156
gperez <gperez@garcia.com.mx>
2019-10-08 09:24:14 -0500

    Se agrega Modal y Toast

---
+++ src/components/RemoteTableStores/RemoteTableStores.jsx
@@ -0,0 +1,183 @@
+import React from 'react';
+import MaterialTable from 'material-table';
+
+export default class RemoteTableStores extends React.Component{
+
+    constructor (props){
+        super(props);
+        this.serverApi = "http://ec2-34-219-142-13.us-west-2.compute.amazonaws.com/";
+        this.methods = {
+            show    : this.serverApi+'api/v1/warehouses/getall?',
+            add     : this.serverApi+'api/v1/warehouses/store?',
+            delete  : this.serverApi+'api/v1/warehouses/destroy?',
+            edit    : this.serverApi+'api/v1/warehouses/update?'
+        }
+        this.query = "";
+        this.state = {
+            columns: [
+                { title: 'ID Tienda',field : "store.id", type: "numeric" },
+                { title: 'Num. Tienda', field: 'store.number', type: 'numeric',editable: 'never'},
+                { title: 'Nombre de la tienda', field: 'store.name',editable: 'never' },
+                { title: 'Num. Bodega (DB)', field: 'id', type: 'numeric' ,editable: 'never'},
+                { title: 'Nombre de la bodega', field: 'name'},
+            ],
+            data :[]
+        }
+        this.headers = {
+            "Accept": "application/json",
+            "Content-Type": "application/json",
+            "Authorization": 'Bearer '+this.props.key_,
+        }
+    }
+
+    show($query){
+        this.query = $query;
+        let $return;
+        $return = new Promise((resolve, reject) => {
+            let $url = this.methods.show;
+            $url += "per_page="+this.query.pageSize+"&page="+(this.query.page+1);
+            let $params = {
+                method  : "get",
+                headers : this.headers
+            }
+            fetch($url,$params)
+            .then(response => response.json())
+            .then(result => {
+                console.log(result);
+                resolve({
+                    data: result.data,
+                    page: (result.current_page-1),
+                    totalCount:result.total
+                });
+                console.log("one Charge");
+            });
+        });
+
+        return $return;
+    }
+
+    add($newData){
+        let $params = {
+            method: "POST",
+            headers: this.headers,
+            body: JSON.stringify($newData)
+        };
+        console.log($newData);
+
+        //let $result = new Promise(resolve => resolve());
+
+        let $result = new Promise(resolve => {
+            setTimeout(() => {
+                let $url = this.methods.add+'store_id='+$newData.store.id;
+                fetch($url,$params)
+                    .then(response => response.json())
+                    .then(result => {
+                        if(!result.success){
+                            //let text = result.error.code;
+                        }
+                        resolve();
+                    })
+                    .catch(error => {
+                        console.log(error);
+                        resolve();
+                    });
+
+            }, 600);
+        });
+
+        return $result;
+    }
+
+    delete($oldData){
+        let $return;
+        $oldData['per_page']= this.query.totalCount;
+        console.log($oldData);
+
+        let $params = {
+            method: "POST",
+            headers: this.headers,
+            body: JSON.stringify($oldData)
+        };
+
+        $return =  new Promise(resolve => {
+            setTimeout(() => {
+                let $url = this.methods.delete+'warehouse_id='+$oldData.id;
+                fetch($url,$params)
+                    .then(response => response.json())
+                    .then(result => {
+                        console.log(result);
+                        if(result.success){
+                            this.cache = {
+                                data: result.data[0].data,
+                                page: result.data[0].current_page-1,
+                                totalCount: result.data[0].total
+                            }
+                        }else{
+
+                        }
+                        resolve(this.cache);
+                    })
+                    .catch(error => {
+                        //toaster.notify(error,{ position: "top-right"});
+                        console.log(error);
+                        resolve(this.cache);
+                    });
+            }, 600);
+        })
+
+        return $return;
+    }
+
+    edit(newData,oldData){
+        let $return;
+        let $params = {
+            method: "POST",
+            headers: this.headers,
+            body: JSON.stringify(newData)
+        };
+
+        $return = new Promise(resolve => {
+            setTimeout(() => {
+                let $url = this.methods.edit+"warehouse_id="+oldData.id+"&name="+newData.name+"&store_id="+newData.store.id;
+                //resolve();
+                fetch($url,$params)
+                    .then(response => response.json())
+                    .then(result => {
+                        if(!result.success){
+
+                        }
+                        resolve();
+                    })
+                    .catch(error => {
+                        //toaster.notify(error,{ position: "top-right"});
+                        console.log(error);
+                        resolve(this.cache);
+                    });
+            }, 600);
+        })
+        return $return;
+    }
+
+    render(){
+        return (
+            <div>
+                <MaterialTable
+                    title={this.props.title}
+                    columns={ this.state.columns }
+                    data={ $query => this.show($query)}
+                    editable={{
+                        onRowAdd: newData => this.add(newData),
+                        onRowUpdate: (newData, oldData) => this.edit(newData,oldData),
+                        onRowDelete: oldData => this.delete(oldData)
+                    }}
+
+                    options={{
+                        pageSize: 10
+                    }}
+                />
+            </div>
+        );
+    }
+}
+
+//ReactDOM.render(<MaterialTableNewDemo />, document.querySelector('#Table'));
---
+++ src/views/Racks/Racks.js
@@ -0,0 +1,24 @@
+/*eslint-disable*/
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+// core components
+import GridItem from "components/Grid/GridItem.js";
+import GridContainer from "components/Grid/GridContainer.js";
+import Card from "components/Card/Card.js";
+import styles from "assets/jss/material-dashboard-react/views/iconsStyle.js";
+import RemoteTableLocations from "components/RemoteTableLocations/RemoteTableLocations.jsx";
+
+const useStyles = makeStyles(styles);
+
+export default function Racks() {
+    const classes = useStyles();
+    return (
+        <GridContainer>
+            <GridItem xs={12} sm={12} md={12}>
+                <Card plain>
+                    <RemoteTableLocations title="Contenido de los anaqueles" key_="eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6IjFiNjM2MWE2MDkzYjRiNzMwZWYxMTY1YzIxODA1ZmYxYWFjNWZhN2JlMjRlMzliZGQ2Yjk2NTA1OTcxZDZjMDgxNjE3Y2Y2YzBlYjUwMWRhIn0.eyJhdWQiOiIxIiwianRpIjoiMWI2MzYxYTYwOTNiNGI3MzBlZjExNjVjMjE4MDVmZjFhYWM1ZmE3YmUyNGUzOWJkZDZiOTY1MDU5NzFkNmMwODE2MTdjZjZjMGViNTAxZGEiLCJpYXQiOjE1NjgwNTAwMDEsIm5iZiI6MTU2ODA1MDAwMSwiZXhwIjoxNTk5NjcyNDAxLCJzdWIiOiIiLCJzY29wZXMiOltdfQ.I-zsnSQ7kvBgwYos9vcvsjsZoRubdhtyxLlSGXhIfO5FVD0qBf2OuxpTrTFaCzZuSt0xaZjBNbRxKC8YfZou4wY0HTFsquz7nfRTBSnyG1O1oI1RkJq3H9MHNdZSASyxd90SzD-hUN_erkQGV2Zx3QJcwBWbBrVrtuxP-VpeeHh2g3X9PnG5GnR5i7mkhFbPSVI6gYQvgbRvXcEMCGHt2ifKYC3cAr43cHUrNDQphYEesD9AxRgdruikVBQ3ZKFSi1Ax80Kr-iPrgaOIQMc17mQZK18x3jfsNNpFgQMWzcaUvJdF60G-DemLQHmnj3CEjSQt42vtwupHMsABGji_HFC0u26F1yuh2FcX1iVQ59UJ2bajYiWuudJt8PawVv0E2OZlb2AWJHa2Hpmt6ZX_TBYMRbuIWdRAU0UOUC5vbsf6tl4dwjAwig36LllWDNGaGZozK4DCyepOHpml35vBn1C9ju5KBKmZygGcULgPN7ehuxMCos8vRleHGx2qaXAZUiwPT55DLI_XjTqQe1R-qxDxqAWws6at0CnM4hiMj5VEl9ptvhzYpVJbd9ytlMV4rVS3woFdAz4APFopHx-nGtno5bbJCheL0NwkLD9JldR--MuZscx2NRhvdiqAkinryqQo3eaBqblTLT9J8z_U4kwwph-X5r_4dNngy1SlUYQ" />
+                </Card>
+            </GridItem>
+        </GridContainer>
+    );
+}
